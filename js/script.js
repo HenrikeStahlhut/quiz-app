ec32bd49b2f7da743d5418eccd6ea3cefbb8989f
Henrike Stahlhut <henrike.st@icloud.com>
2023-03-23 15:16:11 +0100

    add toggle button, hide/show answer on click

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
+const answer = document.querySelector('[data-js="answer"]');
+const showAnswerButton = document.querySelector('[data-js="solutionButton"]');
 
 function bookmarkToggle (){
     bookmarkButton.classList.toggle("bookmark--toogle");
@@ -7,3 +9,19 @@ function bookmarkToggle (){
 bookmarkButton.addEventListener("click",() => {
     bookmarkToggle();
 });
+
+function showAnswer(){
+    if (answer.style.display === "none") {
+        answer.style.display = "block";
+        showAnswerButton.textContent="Hide Answer";
+      } else {
+        showAnswerButton.textContent="Show Answer";
+        answer.style.display = "none";
+      }
+}
+
+showAnswerButton.addEventListener("click", ()=>{
+    showAnswer();
+})
+
+
